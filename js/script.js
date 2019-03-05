52c06a150a5b9cc9d37ba3f760b3ca669d0809d9
Gentian Shkembi <gshkembi@alteanet.it>
2019-03-05 18:00:33 +0100

    finished refactoring index and js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     // data values object
-    var data2 = {
+    var recipe = {
         cannabisWeight: document.querySelector('input[name="cannabisWeight"]'),
         thcPercent: document.querySelector('input[name="thcPercent"]'),
         servingsNr: document.querySelector('input[name="servingsNr"]'),
@@ -12,67 +12,70 @@ document.addEventListener('DOMContentLoaded', function () {
     // iterates over every record in the data object
     // binds event to input
     // calls calculate() function
-    for (var field in data2) {
-        data2[field].addEventListener('keyup', function () {
+    for (var field in recipe) {
+        recipe[field].addEventListener('keyup', function () {
             calculateCannabutter();
         });
-        data2[field].addEventListener('change', function () {
+        recipe[field].addEventListener('change', function () {
             calculateCannabutter();
         });
-        data2[field].addEventListener('click', function () {
+        recipe[field].addEventListener('click', function () {
             calculateCannabutter();
         });
-        data2[field].addEventListener('input', function () {
+        recipe[field].addEventListener('input', function () {
             calculateCannabutter();
         });
     }
 
+    // error fields
     var cannabisWeightError = document.querySelectorAll('.cannabisWeightError')[0];
     var servingsNrError = document.querySelectorAll('.servingsNrError')[0];
 
     function calculateCannabutter() {
 
-        var thcPercent = parseFloat(data2.thcPercent.value);
+        var thcPercent = parseFloat(recipe.thcPercent.value);
 
-        if (data2.cannabisWeight.value > 99) {
+        if (recipe.cannabisWeight.value > 99) {
             cannabisWeightError.style.display = "block";
             cannabisWeightError.innerHTML = "Can't go beyond 99!";
         }
 
-        if (data2.cannabisWeight.value < 0.5) {
+        if (recipe.cannabisWeight.value < 0.5) {
             cannabisWeightError.style.display = "block";
             cannabisWeightError.innerHTML = "Minimum Required Value is 0.5";
         }
 
-        if (data2.servingsNr.value > 99) {
+        if (recipe.cannabisWeight.value <= 99 && recipe.cannabisWeight.value >= 0.5) {
+            cannabisWeightError.style.display = "none";
+        }
+
+        if (recipe.servingsNr.value > 99) {
             servingsNrError.style.display = "block";
             servingsNrError.innerHTML = "Can't go beyond 99!";
         }
 
-        if (data2.cannabisWeight.value <= 99 && data2.cannabisWeight.value >= 0.5) {
-            cannabisWeightError.style.display = "none";
-        }
-
-        if (data2.servingsNr.value <= 99 && data2.servingsNr.value >= 0.5) {
-            servingsNrError.style.display = "none";
-        }
-
-        if (data2.servingsNr.value < 1) {
+        if (recipe.servingsNr.value < 1) {
             servingsNrError.style.display = "block";
             servingsNrError.innerHTML = "Minimum Required Value is 1";
         }
 
-        if (data2.cannabisWeight.value <= 99 && data2.cannabisWeight.value >= 0.5 && data2.servingsNr.value <= 99 && data2.servingsNr.value >= 1) {
-            var thcPerServing = ((data2.cannabisWeight.value * 1000) * (thcPercent / 100) * 0.6) / data2.servingsNr.value;
+
+        if (recipe.servingsNr.value <= 99 && recipe.servingsNr.value >= 0.5) {
+            servingsNrError.style.display = "none";
+        }
+
+        if (recipe.cannabisWeight.value <= 99 && recipe.cannabisWeight.value >= 0.5 && recipe.servingsNr.value <= 99 && recipe.servingsNr.value >= 1) {
+            var thcPerServing = ((recipe.cannabisWeight.value * 1000) * (thcPercent / 100) * 0.6) / recipe.servingsNr.value;
             var thcaToThcPerServing = thcPerServing * 0.88;
 
-            data2.thcPerServing.textContent = thcPerServing.toFixed(1) + " Mg";
-            data2.thcaToThcPerServing.textContent = thcaToThcPerServing.toFixed(1) + " Mg";
+            recipe.thcPerServing.textContent = thcPerServing.toFixed(1) + " Mg";
+            recipe.thcaToThcPerServing.textContent = thcaToThcPerServing.toFixed(1) + " Mg";
         }
 
 
     }
 
+    // range slider functionality
     var rangeSlider = document.getElementById("rs-range-line");
     var rangeBullet = document.getElementById("rs-bullet");
     var rangeFill = document.getElementById("rs-fill");
